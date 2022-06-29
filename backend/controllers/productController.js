5f8d3fd493efff36eaf6e5e57711c59934affff2
Dheeraj Kumar Maurya <dheeraj.dm495@gmail.com>
2022-06-29 20:14:17 +0530

    filter feature for product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -16,7 +16,7 @@ exports.createProduct = catchAsyncError(async (req,res,next)=>{
 //get all product
 exports.getAllProducts = catchAsyncError(async(req,res) =>{
 
-    const apiFeature = new ApiFeatures(Product.find(),req.query).search();
+    const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter();
     const products = await apiFeature.query;
 
     res.status(200).json({
--- backend/utils/apifeatures.js
+++ backend/utils/apifeatures.js
@@ -19,6 +19,23 @@ class ApiFeatures {
         this.query = this.query.find({ ...keyword });
         return this;
     }
+
+    filter() {
+        const queryCopy = { ...this.queryStr };
+        //   Removing some fields for category
+        const removeFields = ["keyword", "page", "limit"];
+    
+        removeFields.forEach((key) => delete queryCopy[key]);
+    
+        // Filter For Price and Rating
+    
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+    
+        this.query = this.query.find(JSON.parse(queryStr));
+    
+        return this;
+      }
 }
 
 
