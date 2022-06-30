119bb412068d6b91c8a4698e3ee4850007bfd980
Dheeraj Kumar Maurya <dheeraj.dm495@gmail.com>
2022-06-30 09:33:23 +0530

    token generation and login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,9 +14,43 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) =>{
             public_id: "this is a sample id",
             url: "profilepicUrl",
         },
-    })
+    });
+
+    const token = user.getJWTToken();
     res.status(201).json({
         success: true,
-        user,
+        token,
     });
 });
+
+// Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+
+    const { email, password } = req.body;
+
+    //checking if user has give password and password both
+
+    if (!email || !password) {
+        return next(new ErrorHander("Please Enter Email & Password", 400));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorHander("Invalid email or password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHander("Invalid email or password", 401));
+    }
+
+    const token = user.getJWTToken();
+
+    res.status(200).json({
+        success: true,
+        token,
+    });
+
+})
