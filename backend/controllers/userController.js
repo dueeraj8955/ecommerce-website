78d27fa139deb20fa386addd9f22a3dd85bc4c20
Dheeraj Kumar Maurya <dheeraj.dm495@gmail.com>
2022-07-01 11:53:00 +0530

    now you reset password successfully

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,5 +104,39 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next)=>{
 
         return next(new ErrorHander(error.message, 500));
     }
-
 });
+
+// Reset Password
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+    // creating token hash
+    const resetPasswordToken = crypto
+      .createHash("sha256")
+      .update(req.params.token)
+      .digest("hex");
+  
+    const user = await User.findOne({
+      resetPasswordToken,
+      resetPasswordExpire: { $gt: Date.now() },
+    });
+  
+    if (!user) {
+      return next(
+        new ErrorHander(
+          "Reset Password Token is invalid or has been expired",
+          400
+        )
+      );
+    }
+  
+    if (req.body.password !== req.body.confirmPassword) {
+      return next(new ErrorHander("Password does not Match", 400));
+    }
+  
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+  
+    await user.save();
+  
+    sendToken(user, 200, res);
+  });
