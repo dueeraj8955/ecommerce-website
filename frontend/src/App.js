d515fd58f15fdb4131c2164460ef350aecc61694
Dheeraj Kumar Maurya <dheeraj.dm495@gmail.com>
2022-07-03 11:25:50 +0530

    frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header  from './component/layout/Header.js';
+import {BrowserRouter as Router} from "react-router-dom"
+
+
+function App() {
+  return (
+    <Router><Header/></Router>
+  );
+}
+
+export default App;
