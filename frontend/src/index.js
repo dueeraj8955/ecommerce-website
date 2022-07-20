c297133bde9e2c6fd5e2ec3548f897f007cf181c
Dheeraj Kumar Maurya <dheeraj.dm495@gmail.com>
2022-07-20 22:42:12 +0530

    successfully imported user reducer from user constants

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
-import {positions,transitions,Provider as AlertProvider} from "react-alert";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import { positions, transitions, Provider as AlertProvider } from "react-alert";
 import AlertTemplate from "react-alert-template-basic";
 
-const option = {
+const options = {
   timeout: 5000,
   position: positions.BOTTOM_CENTER,
   transition: transitions.SCALE,
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <Provider store={store}>
-    <AlertProvider template={AlertTemplate}{...option}>
-      <App/>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <App />
     </AlertProvider>
-    
-  </Provider>
+  </Provider>,
+  document.getElementById("root")
 );
